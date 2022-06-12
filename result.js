4b3ebaa1fab86047ae2e6eccaa3b56c3a84acc74
Mario Lisbona <mario.lisbona@gmail.com>
2022-06-12 15:59:37 +1000

    create message for low user rating

--- result.js
+++ result.js
@@ -1,7 +1,20 @@
 rating = localStorage.getItem('userRating');
 
 const resultElement = document.querySelector('.rating-result');
-const para = document.createElement('p');
-const node = document.createTextNode(`You chose ${rating} out of 5`);
+const thankyouInfo = document.querySelector('.thankyou-info');
+var para = document.createElement('p');
+var node = document.createTextNode(`You chose ${rating} out of 5`);
 para.appendChild(node);
 resultElement.appendChild(para);
+
+
+if (rating == 1 || rating == 2) {
+    node = document.createTextNode("Were sorry your experience wasnt great. We'll be in touch to see how we can improve for next time");
+    para.appendChild(node);
+    thankyouInfo.appendChild(node);
+} else {
+    node = document.createTextNode("We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!");
+    para.appendChild(node);
+    thankyouInfo.appendChild(node);
+}
+
