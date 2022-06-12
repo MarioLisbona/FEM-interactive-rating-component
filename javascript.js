b96a6aed067c9c96c235bf8d78d327d9137d3bb8
Mario Lisbona <mario.lisbona@gmail.com>
2022-06-12 16:31:17 +1000

    change hover state for submit button

--- javascript.js
+++ javascript.js
@@ -1,15 +1,8 @@
 const submit = document.querySelector('.submit');
 
 submit.addEventListener('click', ()=> {
-    submit.style.backgroundColor = 'white';
-    submit.style.color = 'hsl(25, 97%, 53%)';
     let rating = document.querySelector('input[name="rating"]:checked').value;
-    console.log(rating);
     
     localStorage.setItem('userRating', rating);
     window.location.replace("./result.html");
 });
-
-
-
-
